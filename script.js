7c169b36d1ff574b78c104711582000a5becc679
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-22 20:39:43 +0200

    add-button-created-not-working-properly

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 const questionContainer = document.querySelector('.question-container');
 const inputs = document.querySelectorAll('.answer')
-console.log(inputs)
+const buttonNext = document.querySelector('.button-next')
 
 async function printQuestionsAndAnswers() {
     let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
     let data = await response.json();
-    let questions = data.results;
+    let questions = data.results
+    console.log(questions);
 
-    for (let i = 0; i < questions.length; i++) {
-        //Questions
-        let h2 = document.createElement('h2');
-        h2.innerHTML = questions[i].question;
-        questionContainer.appendChild(h2);
+    //Questions
+    let h2 = document.createElement('h2');
+    h2.innerHTML = questions[0].question;
+    questionContainer.appendChild(h2);
 
-        //inputs from correct answers
-        let inputCorrect = document.createElement('input');
-        inputCorrect.setAttribute('type', 'radio');
-        inputCorrect.setAttribute('value', 'correct')
-        questionContainer.appendChild(inputCorrect);
+    //inputs from correct answers
+    let inputCorrect = document.createElement('input');
+    inputCorrect.setAttribute('type', 'radio');
+    inputCorrect.setAttribute('value', 'correct');
+    questionContainer.appendChild(inputCorrect);
 
-        //Correct answer
-        let correctAnswer = document.createElement('label');
-        correctAnswer.innerHTML = questions[i].correct_answer;
-        questionContainer.appendChild(correctAnswer);
+    //Correct answer
+    let correctAnswer = document.createElement('label');
+    correctAnswer.innerHTML = questions[0].correct_answer;
+    questionContainer.appendChild(correctAnswer);
 
-        //incorrect answers
-        let incorrectAnswers = questions[i].incorrect_answers;
-        //esto es un bucle for of, la primera variable es cada elemento, y la segunda es el array completo
-        for (let incorrectAnswer of incorrectAnswers) {
-            //inputs for incorrect answers
-            let inputIncorrect = document.createElement('input');
-            inputIncorrect.setAttribute('type', 'radio');
-            questionContainer.appendChild(inputIncorrect)
+    //incorrect answers
+    let incorrectAnswers = questions[0].incorrect_answers;
+    for (let incorrectAnswer of incorrectAnswers) {
+        //inputs for incorrect answers
+        let inputIncorrect = document.createElement('input');
+        inputIncorrect.setAttribute('type', 'radio');
+        questionContainer.appendChild(inputIncorrect);
 
-            //Incorrect answers printed
-            let pOfIncorrectAnswers = document.createElement('label');
-            pOfIncorrectAnswers.innerText = incorrectAnswer;
-            pOfIncorrectAnswers.setAttribute('value', 'incorrect');
-            questionContainer.appendChild(pOfIncorrectAnswers);
-        }
-    }
+        //Incorrect answers printed
+        let pOfIncorrectAnswers = document.createElement('label');
+        pOfIncorrectAnswers.innerText = incorrectAnswer;
+        pOfIncorrectAnswers.setAttribute('value', 'incorrect');
+        questionContainer.appendChild(pOfIncorrectAnswers);
+    };
 
     // Crear id/for/name a los inputs/label    
     const inputs = document.querySelectorAll('input');
@@ -53,10 +51,64 @@ async function printQuestionsAndAnswers() {
         input.name = 'respuesta';
         input.id = id;
         label.setAttribute('for', id);
-    }
-}
+    };
 
+    //BUTTON NEXT
+    let indexOfQuestionsArray = 1
+    buttonNext.addEventListener('click', () => {
+        let counter = indexOfQuestionsArray++
+
+        //Input correct answer
+        let inputCorrect = document.createElement('input');
+        inputCorrect.setAttribute('type', 'radio');
+        inputCorrect.setAttribute('value', 'correct');
+        questionContainer.appendChild(inputCorrect);
+
+        //next question
+        let h2 = document.createElement('h2');
+        h2.innerHTML = questions[counter].question;
+        questionContainer.appendChild(h2);
+
+        //next correct answer
+        let correctAnswer = document.createElement('label');
+        correctAnswer.innerHTML = questions[counter].correct_answer;
+        questionContainer.appendChild(correctAnswer);
+
+        //next incorrect answers
+        let incorrectAnswers = questions[counter].incorrect_answers;
+        for (let incorrectAnswer of incorrectAnswers) {
+            //inputs for incorrect answers
+            let inputIncorrect = document.createElement('input');
+            inputIncorrect.setAttribute('type', 'radio');
+            questionContainer.appendChild(inputIncorrect);
+
+            //Incorrect answers printed
+            let pOfIncorrectAnswers = document.createElement('label');
+            pOfIncorrectAnswers.innerText = incorrectAnswer;
+            pOfIncorrectAnswers.setAttribute('value', 'incorrect');
+            questionContainer.appendChild(pOfIncorrectAnswers);
+        };
+
+        // Crear id/for/name a los inputs/label    
+        const inputs = document.querySelectorAll('input');
+        const labels = document.querySelectorAll('label');
+
+
+        for (let i = 0; i < inputs.length; i++) {
+            const id = (i + 1).toString();
+            const input = inputs[i];
+            const label = labels[i];
+
+            input.name = 'respuesta';
+            input.id = id;
+            label.setAttribute('for', id);
+        };
+    });
+};
 printQuestionsAndAnswers();
 
-//lo siguiente es conseguir el resultado. El resultado va a ser el numero de inputs en estado checked que tengan el value true. antes de nada hay que meterle un atributo value a las respuesta correctas de true, y a las incorrectas un valor de false
+
+
+
+
 
