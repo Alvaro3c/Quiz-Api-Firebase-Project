9432c82dc7ba20c3233cbbd3006f8f74eafbe783
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-23 19:41:22 +0200

    fix/not-finished

--- script.js
+++ script.js
@@ -5,33 +5,38 @@ const article = document.querySelector('article');
 
 
 async function printQuestionsAndAnswers() {
-    let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
-    let data = await response.json();
-    let questions = data.results;
 
-    function randomArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        };
-    };
-    //Unir en un array la respuesta correcta y las incorrectas
-    let arrayAllAnswers = [];
-    for (let i = 0; i < questions.length; i++) {
-        let arrayAnswers = []
-        arrayAnswers.push(questions[i].correct_answer);
+    try {
+        let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
+        let data = await response.json();
+        let objQuestions = data.results;
+        for 
+        let questions = data.results.question;
+            let correctAnswer = objQuestions[0].correct_answer;
+        let incorrectAnswer = objQuestions[0][incorrect_answers];
+        console.log(questions);
+        console.log(correctAnswer);
+        console.log(incorrectAnswer);
 
-        for (let j = 0; j < 3; j++) {
-            arrayAnswers.push(questions[i].incorrect_answers[j])
+        // let objectDePutaMadre = [{
+        //     correctAnswer: `${questions[correct_answer]}`,
+        //     incorrectAnswert: `${questions[incorrect_answers]}`,
+        //     question: `${questions.question}`
+        // }];
 
-        }
-        randomArray(arrayAnswers)//Esto retorna un array randomizado de COJONES 0P0
-        arrayAllAnswers.push(arrayAnswers)
+        // console.log(objectDePutaMadre);
+
+
+
+
+
+
+
     }
-
-    let print = ``
-
-    // funcion para randomizar las respuestas. Eto no ze toca
+    catch (error) {
+        console.error(error)
+    }
+    //Unir en un array la respuesta correcta y las incorrectas
 
 
 }
