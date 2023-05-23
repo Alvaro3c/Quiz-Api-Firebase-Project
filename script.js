1cee2a312c5070833d6d44779e7a2ec02d8e9b5b
Rebeca <becabecks3.gmail.com>
2023-05-23 19:44:14 +0200

    add/localStorage-print

--- script.js
+++ script.js
@@ -1,191 +1,47 @@
-const questionContainer = document.querySelector('.question-container');
-const inputs = document.getElementsByTagName('input')
-let questions = [];
-
-
 async function printQuestionsAndAnswers() {
-    let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
-    let data = await response.json();
 
+  try {
+      let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
+      let data = await response.json();
+      let objQuestions = data.results;
 
-    let questions = data.results;
+      // let correctCheck = [];
+      // for (let i = 0; i < objQuestions.length; i++){
+      //   correctCheck.push(question[i].correct_answer)
+      // }
 
-    console.log(questions);
+      let getInfo = objQuestions.map(question => ({
+        question: question.question,
+        correctAnswer: question.correct_answer,
+        incorrectAnswers: question.incorrect_answers
+      }));
 
-    //Questions
-    let h2 = document.createElement('h2');
-    h2.innerHTML = questions[0].question;
-    questionContainer.appendChild(h2);
-
-    //inputs from correct answers
-    let inputCorrect = document.createElement('input');
-    inputCorrect.setAttribute('type', 'radio');
-    inputCorrect.setAttribute('value', 'correct');
-    questionContainer.appendChild(inputCorrect);
-
-    //Correct answer
-    let correctAnswer = document.createElement('label');
-    correctAnswer.innerHTML = questions[0].correct_answer;
-    questionContainer.appendChild(correctAnswer);
-
-    //incorrect answers
-    let incorrectAnswers = questions[0].incorrect_answers;
-    for (let incorrectAnswer of incorrectAnswers) {
-        //inputs for incorrect answers
-        let inputIncorrect = document.createElement('input');
-        inputIncorrect.setAttribute('type', 'radio');
-        questionContainer.appendChild(inputIncorrect);
-
-        //Incorrect answers printed
-        let pOfIncorrectAnswers = document.createElement('label');
-        pOfIncorrectAnswers.innerText = incorrectAnswer;
-        pOfIncorrectAnswers.setAttribute('value', 'incorrect');
-        questionContainer.appendChild(pOfIncorrectAnswers);
-    };
-
-    // Crear id/for/name a los inputs/label    
-    const inputs = document.querySelectorAll('input');
-    const labels = document.querySelectorAll('label');
-
-    for (let i = 0; i < inputs.length; i++) {
-        const id = (i + 1).toString();
-        const input = inputs[i];
-        const label = labels[i];
-
-        input.name = 'respuesta';
-        input.id = id;
-        label.setAttribute('for', id);
-    };
-
-    //BUTTON NEXT
-    let indexOfQuestionsArray = 1
-    buttonNext.addEventListener('click', () => {
-        let counter = indexOfQuestionsArray++
-
-        //Input correct answer
-        let inputCorrect = document.createElement('input');
-        inputCorrect.setAttribute('type', 'radio');
-        inputCorrect.setAttribute('value', 'correct');
-        questionContainer.appendChild(inputCorrect);
-
-        //next question
-        let h2 = document.createElement('h2');
-        h2.innerHTML = questions[counter].question;
-        questionContainer.appendChild(h2);
-
-        //next correct answer
-        let correctAnswer = document.createElement('label');
-        correctAnswer.innerHTML = questions[counter].correct_answer;
-        questionContainer.appendChild(correctAnswer);
-
-        //next incorrect answers
-        let incorrectAnswers = questions[counter].incorrect_answers;
-        for (let incorrectAnswer of incorrectAnswers) {
-            //inputs for incorrect answers
-            let inputIncorrect = document.createElement('input');
-            inputIncorrect.setAttribute('type', 'radio');
-            questionContainer.appendChild(inputIncorrect);
-
-            //Incorrect answers printed
-            let pOfIncorrectAnswers = document.createElement('label');
-            pOfIncorrectAnswers.innerText = incorrectAnswer;
-            pOfIncorrectAnswers.setAttribute('value', 'incorrect');
-            questionContainer.appendChild(pOfIncorrectAnswers);
-        };
-
-        // Crear id/for/name a los inputs/label    
-        const inputs = document.querySelectorAll('input');
-        const labels = document.querySelectorAll('label');
-
-
-
-        for (let i = 0; i < inputs.length; i++) {
-            const id = (i + 1).toString();
-            const input = inputs[i];
-            const label = labels[i];
-
-            input.name = 'respuesta';
-            input.id = id;
-            label.setAttribute('for', id);
-        };
-    });
-};
-printQuestionsAndAnswers();
-
-
-
-
-
-
-
-    // Crear id/for/name a los inputs/label    
-    const inputs = document.querySelectorAll('input');
-    const labels = document.querySelectorAll('label');
-    
-    for (let i = 0; i < inputs.length; i++) {
-      const id = (i + 1).toString();
-      const input = inputs[i];
-      const label = labels[i];
+      // let storeData = {
+      //   correctCheck,
+      //   getInfo
+      // }
       
-      input.name = 'respuesta';
-      input.id = id;
-      label.setAttribute('for', id);
-    }
-}
-console.log(inputs);
-printQuestionsAndAnswers();
+      localStorage.setItem('questionsData', JSON.stringify(getInfo)); //Aqui iria storeData, pero no funciona
 
-//Comprobar seleccion usuario
+      let article = document.querySelector('.question');
 
-// Función para obtener las respuestas seleccionadas por el usuario y las respuestas correctas
-function getSelectedAnswers() {
-    const selectedAnswers = [];
-    const inputs = document.querySelectorAll('input[type="radio"]:checked');
-  
-    for (let i = 0; i < inputs.length; i++) {
-      const input = inputs[i];
-      const questionIndex = parseInt(input.getAttribute('name').split('_')[1]);
-      const answer = input.value;
-  
-      selectedAnswers.push({
-        questionIndex: questionIndex,
-        answer: answer
+      let print = '';
+      getInfo.forEach((question, index) => {
+        print +=  `<h2>${question.question}</h2>
+        <input type="radio" id="answer_${index}_correct" name="answer_${index}" value="${question.correctAnswer}">
+        <label for="answer_${index}_correct" >${question.correctAnswer}</label>
+        <input type="radio" id="answer_${index}_incorrect1" name="answer_${index}" value="${question.incorrectAnswers[0]}">
+        <label for="answer_${index}_incorrect1" >${question.incorrectAnswers[0]}</label>
+        <input type="radio" id="answer_${index}_incorrect2" name="answer_${index}" value="${question.incorrectAnswers[1]}">
+        <label for="answer_${index}_incorrect2" >${question.incorrectAnswers[1]}</label>
+        <input type="radio" id="answer_${index}_incorrect3" name="answer_${index}" value="${question.incorrectAnswers[2]}">
+        <label for="answer_${index}_correct3" >${question.incorrectAnswers[2]}</label>`;
       });
+      article.innerHTML = print;
     }
-  
-    return selectedAnswers;
-  }
+    catch (error) {
+      console.error(error)
+    }
 
- 
-  // Función para comprobar las respuestas seleccionadas por el usuario y las respuestas correctas
-  function checkUserAnswers() {
-    const selectedAnswers = getSelectedAnswers();
-    const userResults = [];
-    let score = 0;
-  
-    for (let i = 0; i < selectedAnswers.length; i++) {
-      const selectedAnswer = selectedAnswers[i];
-      const question = questions[selectedAnswer.questionIndex];
-  
-      if (question) {
-        const correctAnswer = question.correct_answer;
-  
-        const isCorrect = selectedAnswer.answer === correctAnswer;
-  
-        userResults.push({
-          questionIndex: selectedAnswer.questionIndex,
-          userAnswer: selectedAnswer.answer,
-          correctAnswer: correctAnswer,
-          isCorrect: isCorrect
-        });
-        article.innerHTML = print;
-      }
-    }
-  
-    console.log(score);
-    console.log(userResults);
-  }
-  
-  // Ejecutar la función para comprobar las respuestas cuando sea necesario, por ejemplo, cuando el usuario haga clic en un botón "Comprobar respuestas"
-  const checkButton = document.querySelector('.button-next');
-  checkButton.addEventListener('click', checkUserAnswers);
+}
+printQuestionsAndAnswers();
