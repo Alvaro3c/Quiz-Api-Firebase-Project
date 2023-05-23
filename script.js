08311a3b497bbb6f22dd1f4f53b31e380c7fe617
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-23 16:44:50 +0200

    Array-of-arrays-of-answers-is-done

--- script.js
+++ script.js
@@ -2,44 +2,39 @@ const questionContainer = document.querySelector('.question-container');
 const buttonNext = document.querySelector('.button-next')
 const inputs = document.querySelectorAll('input');
 const article = document.querySelector('article');
-console.log(article)
+
 
 async function printQuestionsAndAnswers() {
     let response = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
     let data = await response.json();
-    let questions = data.results
-    console.log(questions);
-
-    let answersOfOneQuestion = []
-
-    answersOfOneQuestion.push(questions[0].correct_answer)
-    console.log(questions[0].incorrect_answers) //array de preguntas incorrectas
-
-    //array de preguntas incorrectas iterandose
-    for (let i = 0; i < questions[0].incorrect_answers.length; i++) {
-        /*         let h2 = document.createElement('h2');
-                h2.innerText = questions[i].question;
-                article.appendChild(h2); */
-        answersOfOneQuestion.push(questions[0].incorrect_answers[i])
-    }
-
-    console.log(answersOfOneQuestion);
+    let questions = data.results;
 
     function randomArray(array) {
         for (var i = array.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
+        };
+    };
+    //Unir en un array la respuesta correcta y las incorrectas
+    let arrayAllAnswers = [];
+    for (let i = 0; i < questions.length; i++) {
+        let arrayAnswers = []
+        arrayAnswers.push(questions[i].correct_answer);
+
+        for (let j = 0; j < 3; j++) {
+            arrayAnswers.push(questions[i].incorrect_answers[j])
 
         }
+        randomArray(arrayAnswers)//Esto retorna un array randomizado de COJONES 0P0
+        arrayAllAnswers.push(arrayAnswers)
     }
-    randomArray(answersOfOneQuestion)
-    console.log(answersOfOneQuestion);
+
+
+
+    // funcion para randomizar las respuestas. Eto no ze toca
+
 
 }
-
-
-//BUTTON NEXT
-
 
 printQuestionsAndAnswers();
 
