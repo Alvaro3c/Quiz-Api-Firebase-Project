df779d1a44bd0bf4361f6ab1c9e8db6178a8e025
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-27 11:14:54 +0200

    /fix-array-of-array-of-objects-turned-into-array-of-objects

--- script.js
+++ script.js
@@ -106,13 +106,13 @@ function showNextQuestion() {
         if (isAnswerCorrect) {
             score++;
         }
-        const currentDate = new Date().toLocaleDateString();
-        const gameData = {
-            score: score,
-            date: currentDate
-        };
 
-        localStorage.setItem('gameData', JSON.stringify(gameData));
+        const currentDate = new Date().toLocaleDateString();
+        const gameData = [{
+            score: score,
+            date: currentDate,
+        }];
+
         console.log(score);
         currentIndex++;
         if (currentIndex < questions.length) {
@@ -123,9 +123,21 @@ function showNextQuestion() {
     }
     function handleLastQuestion() {
         nextButton.innerHTML = '<a href="results.html">Show results</a>'
+        //local storage
+        const currentDate = new Date().toLocaleDateString();
+        const gameData = {
+            score: score,
+            date: currentDate,
+        };
+        let scoresData = [];
+        if (localStorage.getItem('gameData')) {
+            scoresData = JSON.parse(localStorage.getItem('gameData'));
+        }
+        scoresData.push(gameData);
+        localStorage.setItem('gameData', JSON.stringify(scoresData));
     }
-
     //BUTTON NEXT
+
     let nextButton = document.querySelector('.button-next');
     nextButton.addEventListener('click', () => {
         if (currentIndex < questions.length - 1) {
@@ -156,34 +168,89 @@ let obj = {
 const getData = JSON.parse(localStorage.getItem('gameData'));
 console.log(getData)
 const dates = getData.map(gameData => gameData.date);
+console.log(dates)
 const scores = getData.map(gameData => gameData.score);
-console.log(scores);
+console.log(scores)
 
-
-
-
-/* 
-var chartData = {
+var data = {
     labels: dates,
     series: [scores]
 };
-var options = {
-    width: 300,
-    height: 200
-};
-new Chartist.Line('.ct-chart', chartData, options); */
-
-var data = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    series: [
-        [5, 2, 4, 2, 0]
-    ]
-};
 
 var options = {
-    width: 300,
-    height: 200
+    width: 1200,
+    height: 800,
+    high: 10,
+    low: 0,
+    axisY: {
+        onlyInteger: true,
+        offset: 20
+    }
 };
-new Chartist.Line('.ct-chart', data, options);
+
+var responsiveOptions = [
+    ['screen and (min-width: 641px) and (max-width: 1024px)', {
+        showPoint: false,
+        axisX: {
+            labelInterpolationFnc: function (value) {
+                // Will return Mon, Tue, Wed etc. on medium screens
+                return value.slice(0, 3);
+            }
+        }
+    }],
+    ['screen and (max-width: 640px)', {
+        showLine: false,
+        axisX: {
+            labelInterpolationFnc: function (value) {
+                // Will return M, T, W etc. on small screens
+                return value[0];
+            }
+        }
+    }]
+];
+
+var defaultOptions = {
+    // Options for X-Axis
+    axisX: {
+        // Allows you to correct label positioning on this axis by positive or negative x and y offset.
+        // If labels should be shown or not
+        showLabel: true,
+        // If the axis grid should be drawn or not
+        showGrid: false,
+    },
+
+    // Specify a fixed width for the chart as a string (i.e. '100px' or '50%')
+    width: '50%',
+    // Specify a fixed height for the chart as a string (i.e. '100px' or '50%')
+    height: '50%',
+    // If the line should be drawn or not
+    showLine: true,
+    // If dots should be drawn or not
+    showPoint: true,
+    // The base for the area chart that will be used to close the area shape (is normally 0)
+    areaBase: 0,
+    // Padding of the chart drawing area to the container element and labels as a number or padding object {top: 5, right: 5, bottom: 5, left: 5}
+    chartPadding: {
+        top: 150,
+        right: 15,
+        bottom: 5,
+        left: 10
+    },
+
+    // Override the class names that get used to generate the SVG structure of the chart
+    classNames: {
+        chart: 'ct-chart-line',
+    }
+}
+
+
+new Chartist.Line('.ct-chart', data, options, null, responsiveOptions, defaultOptions);
+
+
+
+
+
+
+
 
 
